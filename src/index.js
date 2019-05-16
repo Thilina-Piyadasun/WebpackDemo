e8f29340e94dd39ad31df9a1ee76bf16cc7b650f
thilinap <thilinap@codegen.net>
2019-05-16 17:14:52 +0530

    webpack dev server configured

--- src/index.js
+++ src/index.js
@@ -1,4 +1,19 @@
 import flatArray from "./arr";
 
-var array = [ 1,2,3,[4,5,6,[7,8,9]]];
+var array = [ 1,2,3,[4,5,6,[7,8,9,[10,11,12,13]]]];
 console.log(flatArray(array));
+
+
+//webpack dev server create main.js file and serves that file from memory
+/***
+ * i ｢wds｣: webpack output is served from /
+ i ｢wdm｣: Hash: 45875bf3389f8884ced1
+ Version: webpack 4.31.0
+ Time: 314ms
+ Built at: 05/16/2019 5:01:26 PM
+ Asset     Size  Chunks             Chunk Names
+ main.js  349 KiB    main  [emitted]  main
+ Entrypoint main = main.js
+
+ */
+
